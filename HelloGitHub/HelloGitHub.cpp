8bfa3a0bcb82aaed897e0bd6348953c147d9f6aa
David Seguer Pes <david.seguer@enti.cat>
2020-02-13 11:53:41 +0100

    Primer Ejercicio

---
+++ HelloGitHub/HelloGitHub.cpp
@@ -0,0 +1,11 @@
+// HelloGitHub.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello GitHub!\n"; 
+}
+
